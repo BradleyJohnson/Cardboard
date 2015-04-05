afa0438e8edc58a94764197af9c4ee64414258ba
Bradley D. Johnson <bradley.david.johnson@gmail.com>
2015-04-05 12:12:59 -0700

    Big cleanup of sass file. Refactored styling for buttons and links.

--- app/helpers/join_group_button_helper.rb
+++ app/helpers/join_group_button_helper.rb
@@ -1,5 +1,5 @@
 module JoinGroupButtonHelper
   def render_join_button
-    return link_to "Join", {:controller => "memberships", :action => "create", :group_id => @group.id }, class: "btn btn-success outline" if !@membership
+    return link_to "Join", {:controller => "memberships", :action => "create", :group_id => @group.id }, class: "btn btn-primary outline" if !@membership
   end
 end
